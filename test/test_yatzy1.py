04c3d24d5fe79ef9819321ac2d7bf739a84349ef
KanekiiCat <alex123viri@gmail.com>
2025-01-13 11:08:58 +0100

    test(YATZY): :white_check_mark: Fixed existing tests.

--- test/test_yatzy1.py
+++ test/test_yatzy1.py
@@ -108,23 +108,23 @@ def test_two_Pair():
     '''
     Añadido test nuevo
     '''
-    assert 16 == Yatzy().two_pair(3, 3, 5, 4, 5)
-    assert 18 == Yatzy().two_pair(3, 3, 6, 6, 6)
-    assert 0 == Yatzy().two_pair(3, 3, 6, 5, 4)
-    assert 0 == Yatzy().two_pair(6, 3, 6, 6, 6)
+    assert 16 == Yatzy.two_pair(3, 3, 5, 4, 5)
+    assert 18 == Yatzy.two_pair(3, 3, 6, 6, 6)
+    assert 0 == Yatzy.two_pair(3, 3, 6, 5, 4)
+    assert 0 == Yatzy.two_pair(6, 3, 6, 6, 6)
 
 
 def test_three_of_a_kind():
     '''
     Añadido test nuevo
     '''
-    assert 0 == Yatzy().three_of_a_kind(2, 1, 3, 4, 5)
-    assert 9 == Yatzy().three_of_a_kind(3, 3, 3, 4, 5)
-    assert 15 == Yatzy().three_of_a_kind(5, 3, 5, 4, 5)
+    assert 0 == Yatzy.three_of_a_kind(2, 1, 3, 4, 5)
+    assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 4, 5)
+    assert 15 == Yatzy.three_of_a_kind(5, 3, 5, 4, 5)
     assert 9 == Yatzy.three_of_a_kind(3, 3, 3, 3, 5)
 
 
-def test_four_of_a_knd():
+def test_four_of_a_kind():
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
     assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
     assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
@@ -134,12 +134,12 @@ def test_four_of_a_knd():
 def test_smallStraight():
     assert 15 == Yatzy.smallStraight(1, 2, 3, 4, 5)
     assert 15 == Yatzy.smallStraight(2, 3, 4, 5, 1)
-    assert 0 == Yatzy().smallStraight(1, 2, 2, 4, 5)
+    assert 0 == Yatzy.smallStraight(1, 2, 2, 4, 5)
 
 
 def test_largeStraight():
     assert 20 == Yatzy.largeStraight(6, 2, 3, 4, 5)
-    assert 20 == Yatzy().largeStraight(2, 3, 4, 5, 6)
+    assert 20 == Yatzy.largeStraight(2, 3, 4, 5, 6)
     assert 0 == Yatzy.largeStraight(1, 2, 2, 4, 5)
 
 
